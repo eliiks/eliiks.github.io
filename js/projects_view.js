894f033d7f66a535768ddd284c05952dabc4abb5
eliiks <eliiks.rs@gmail.com>
2025-02-24 14:46:02 +0100

    Add more projects + html/css fix

---
+++ js/projects_view.js
@@ -0,0 +1,20 @@
+project_div = document.getElementById("projets")
+view_more_button = document.getElementById("view_more_button")
+view_more = false;
+
+for (let i = 4 ; i < project_div.children.length ; i++){
+    project_div.children[i].classList.add("hidden")
+}
+
+view_more_button.addEventListener("click", () => {
+    view_more = !view_more;
+    for (let i = 4 ; i < project_div.children.length ; i++){
+        project_div.children[i].classList.toggle("hidden")
+    }
+    if(view_more){
+        view_more_button.innerHTML = "Voir moins de projets"
+    }else{
+        view_more_button.innerHTML = "Voir plus de projets"
+    }
+    
+})
